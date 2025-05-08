6eab96dbf49f0a958ff481985241e1425fc3e8a7
Mario Fletcher <mariojfletcher@gmail.com>
2025-05-08 12:20:01 -0500

    Add files via upload

---
+++ system/hardening.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "[*] Applying basic system hardening..."
+sudo apt update && sudo apt upgrade -y
+sudo apt install unattended-upgrades -y
+sudo dpkg-reconfigure --priority=low unattended-upgrades
